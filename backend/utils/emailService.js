37b031bc7517fd760ebdaed0c7c49802fb2956c7
Arambh Tiwari <arambh.tiwari11@gmail.com>
2026-05-30 15:17:06 +0530

    debug email env

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -1,5 +1,8 @@
 import nodemailer from "nodemailer";
 
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
